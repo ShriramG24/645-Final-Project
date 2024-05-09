3635e57ce276972aa127570945f0d98d0e0cacd1
shriramg24 <shrirambhavin@gmail.com>
2024-05-09 14:06:14 -0400

    Add split view

--- src/main.py
+++ src/main.py
@@ -1,44 +1,55 @@
 import numpy as np
 import matplotlib as plt
 from utils import *
-from view import View
 from db import Database
+from scipy.stats import entropy
 
-DATA_PATH = '../data/adult-data.csv'
+DATA_PATH = '../data/adult_data.csv'
 COLUMNS = [
-    'age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship',
-    'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'class'
+    'age', 'workclass', 'fnlwgt', 'education', 'education_num', 'marital_status', 'occupation', 'relationship',
+    'race', 'sex', 'capital_gain', 'capital_loss', 'hours_per_week', 'native_country', 'class'
 ]
 
 DB_NAME = 'project3'
 TABLE_NAME = 'census'
 
 ATTRIBUTES = [
-    'age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship',
-    'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'class'
+    'workclass', 'education', 'occupation', 'relationship', 'race', 'sex', 'native_country', 'class'
 ]
-MEASURES = [ 'age', 'fnlwgt', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week' ]
+MEASURES = [ 'age', 'fnlwgt', 'education_num', 'capital_gain', 'capital_loss', 'hours_per_week' ]
 AGGREGATES = [ 'AVG', 'SUM', 'COUNT', 'MIN', 'MAX' ]
 
 # n: Number of phases
 # k: Top k visualizations will be returned
-def rankVisualizations(n = 10, k = 5):
+def rankVisualizations(database: Database, n = 10, k = 5):
     views = generateInitialViews(ATTRIBUTES, MEASURES, AGGREGATES)
-
+    utilities = { (a, m, f): 0 for a, [m], [f] in views }
     for i in range(n):
         partitionNum = i
         combinedViews = []
         for a in ATTRIBUTES:
-            currViews = list(filter(lambda v: v.groupByAttr == a, views))
+            currViews = list(filter(lambda v: v[0] == a, views))
             combinedViews.append(combineAggregrates(currViews))
 
+        for view in combinedViews[:1]:
+            a, m, f = view
+            data = database.getViewCombinedData(view, partitionNum)
+            values = database.getValues(a)
+            targetVecs, referenceVecs = splitView(view, values, data)
+            for i in range(len(m)):
+                utilities[(a, m[i], f[i])] += entropy(targetVecs[i].values(), referenceVecs[i].values())
+
         # Perform pruning
+        views = pruneViews(combinedViews)
 
 def main():
     db = Database(DB_NAME, TABLE_NAME)
+    db.setupTables()
 
-    N, K = 10, 5
-    # print(rankVisualizations(N, K))
+    N, K = 1, 5
+    rankVisualizations(db, N, K)
+
+    db.closeConnection()
 
 if __name__ == "__main__":
     main()
--- src/utils.py
+++ src/utils.py
@@ -1,25 +1,44 @@
-from view import View
+def splitView(view, values, data):
+    _, measures, _ = view
+    # m = [m1, m2, ...]
+    # f = [f1, f2, ...]
+    # targetVecs = [ f1(m1): { a1: v1, a2: v2, ... }, f2(m2): { } ]
+    targetVecs = [ { k: 1e-5 for k in values } for _ in range(len(measures)) ]
+    referenceVecs = [ { k: 1e-5 for k in values } for _ in range(len(measures)) ]
+    for row in data:
+        # Target Rows
+        attrVal = row[0]
+        if row[1] == 1:
+            for i in range(2, len(row)):
+                targetVecs[i - 2][attrVal] = float(max(row[i], 1e-5))
+        # Reference Rows
+        else:
+            for i in range(2, len(row)):
+                referenceVecs[i - 2][attrVal] = float(max(row[i], 1e-5))
+    
+    return targetVecs, referenceVecs
 
 def generateInitialViews(attrs, measures, aggFuncs):
     allViews = []
     for a in attrs:
         for m in measures:
             for f in aggFuncs:
-                allViews.append(View(a, [m], [f]))
+                allViews.append((a, [m], [f]))
     
     return allViews
 
-def combineAggregrates(views: list[View]):
-    groupByAttr = views[0].groupByAttr
-    measures, aggFuncs = [], []
+def combineAggregrates(views: list):
+    groupByAttr = views[0][0]
+    allMeasures, allAggFuncs = [], []
     for view in views:
-        measures.extend([m for m in view.measures if m not in measures])
-        aggFuncs.extend([f for f in view.aggFuncs if f not in aggFuncs])
+        groupByAttr, measures, aggFuncs = view
+        allMeasures.extend([m for m in measures])
+        allAggFuncs.extend([f for f in aggFuncs])
 
-    return View(groupByAttr, measures, aggFuncs)
+    return (groupByAttr, allMeasures, allAggFuncs)
 
 def pruneViews(views):
     pass
 
-def generateVisualization(view: View):
+def generateVisualization(view):
     pass
