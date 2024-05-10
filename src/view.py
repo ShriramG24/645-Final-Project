af0cd56ce6053f2e38ffced3f767be7e9a322cca
shriramg24 <shrirambhavin@gmail.com>
2024-05-09 22:04:53 -0400

    Remove view.py

--- src/view.py
+++
@@ -1,7 +0,0 @@
-# import numpy as np
-
-# class View:
-#     def __init__(self, a: str, m: list, f: list) -> None:
-#         self.groupByAttr = a
-#         self.measures = m
-#         self.aggFuncs = f
